5b502741db8e401762dca36f551b1fcb37360c00
kittitater <kittitater@gmail.com>
2024-08-28 17:31:40 +0700

    Update page.js

--- robot-monitor-frontend/src/app/page.js
+++ robot-monitor-frontend/src/app/page.js
@@ -245,10 +245,10 @@ export default function Home() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-             Robot Monitoring System
+             Welcome to our Robot Monitoring System
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-            Our Security Guard Assistance Robot Monitoring Web Application. Curious with our project ? Hit the learn more to read our project idea proposal
+            Our Security Guard Assistance Robot Monitoring Web Application.<br></br> Curious with our project ? Hit the learn more to read our project idea proposal.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
