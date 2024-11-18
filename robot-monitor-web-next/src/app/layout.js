0d1129679c6422e1fa4817ea4f3faa705646b89a
Kittitat Songsakseree <kittitater@gmail.com>
2024-11-18 11:36:09 +0700

    Add favicon link to RootLayout

--- robot-monitor-web-next/src/app/layout.js
+++ robot-monitor-web-next/src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/monitor.png" />
+      </head>
       <body>{children}</body>
     </html>
   );
