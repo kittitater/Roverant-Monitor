bef708cfd0b01f33c638ac688fc7fb53f582d0dd
Kittitat Songsakseree <kittitater@gmail.com>
2024-12-20 17:23:10 +0700

    Update footer links and copyright information in Footer component

--- robot-monitor-web-next/src/app/page.js
+++ robot-monitor-web-next/src/app/page.js
@@ -274,9 +274,15 @@
 // }
 
 //import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+'use client'
 
 import Link from "next/link";
 import Footer from "../components/footer";
+
+import { useState } from "react";
+import { ChevronDownIcon } from '@heroicons/react/16/solid'
+import { Field, Label, Switch } from '@headlessui/react'
+
 
 const people = [
   {
@@ -309,7 +315,22 @@ const people = [
   // More people...
 ];
 
+const links = [
+  { name: 'Open roles', href: '#' },
+  { name: 'Internship program', href: '#' },
+  { name: 'Our values', href: '#' },
+  { name: 'Meet our leadership', href: '#' },
+]
+const stats = [
+  { name: 'Offices worldwide', value: '12' },
+  { name: 'Full-time colleagues', value: '300+' },
+  { name: 'Hours per week', value: '40' },
+  { name: 'Paid time off', value: 'Unlimited' },
+]
+
 export default function Home() {
+  const [agreed, setAgreed] = useState(false)
+
   return (
     <>
       <div className="overflow-hidden bg-white py-0 sm:py-22">
@@ -361,7 +382,7 @@ export default function Home() {
         <div className="bg-white  sm:pt-16 sm:pb-32">
           <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
             <div className="max-w-2xl">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <h2 className="text-4xl font-semibold tracking-tight text-gray-900 ">
                 Meet our Innovator
               </h2>
               <p className="mt-6 text-lg leading-8 text-gray-600">
@@ -396,6 +417,166 @@ export default function Home() {
             </ul>
           </div>
         </div>
+
+
+
+        <div id="About" className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+            className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center"
+          />
+          <div
+            aria-hidden="true"
+            className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            />
+          </div>
+          <div
+            aria-hidden="true"
+            className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            />
+          </div>
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:mx-0">
+              <h2 className="text-5xl font-semibold tracking-tight text-white ">About Roverant</h2>
+              <p className="mt-8 text-pretty text-lg font-medium text-white ">
+                Roverant is an innovative security solution designed to enhance safety and efficiency in indoor environments. This mobile robot autonomously patrols designated areas, leveraging advanced technologies like Simultaneous Localization and Mapping (SLAM), YOLO-based object detection, and integrated sensors. Its primary objective is to detect unauthorized individuals, suspicious objects, and abnormal activities while providing real-time video feeds and alerts to security personnel via a web application.
+              </p>
+              <p className="mt-8 text-pretty text-lg font-medium text-white ">
+                The project combines robust hardware, including the NVIDIA Jetson TX2 and LIDAR, with cutting-edge software technologies such as FastAPI and Next.js. The web-based interface enables live monitoring, control, and reporting, ensuring seamless communication between the robot and human operators. Designed for facilities like offices, campuses, medical department, and co-working spaces, Roverant aims to improve security coverage, reduce human error, and streamline security operations.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div id="Contact" className="isolate flex flex-row bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-left text-5xl font-semibold tracking-tight text-gray-900">Contact Roverant</h2>
+            <p className="mt-6 text-lg text-gray-600">You can either get in touch with us via information or fill in the form </p>
+          </div>
+          <form action="#" method="POST" className="mx-auto max-w-xl ">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div className="sm:col-span-2">
+                <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+                  How can we called you
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="name"
+                    className="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-black"
+                  />
+                </div>
+              </div>
+              <div >
+                <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-black"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
+                  Phone number
+                </label>
+                <div className="mt-2.5">
+                  <div className="flex rounded-xl bg-white outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-black">
+                    
+                    <input
+                      id="phone-number"
+                      name="phone-number"
+                      type="text"
+                      maxLength={10}
+                      placeholder="123-456-7890"
+                      autoComplete="tel"
+                      className="block min-w-0 grow py-2 px-3.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={4}
+                    className="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-black"
+                    defaultValue={''}
+                  />
+                </div>
+              </div>
+              <Field className="flex gap-x-4 sm:col-span-2">
+                <div className="flex h-6 items-center">
+                  <Switch
+                    checked={agreed}
+                    onChange={setAgreed}
+                    className="group flex w-8 flex-none cursor-pointer rounded-full bg-gray-200 p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black data-[checked]:bg-black"
+                  >
+                    <span className="sr-only">Agree to policies</span>
+                    <span
+                      aria-hidden="true"
+                      className="size-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out group-data-[checked]:translate-x-3.5"
+                    />
+                  </Switch>
+                </div>
+                <Label className="text-sm/6 text-gray-600">
+                  By selecting this, you agree to our{' '}
+                  <a href="#" className="font-semibold text-gray-900">
+                    privacy&nbsp;policy
+                  </a>
+                  .
+                </Label>
+              </Field>
+            </div>
+            <div className="mt-10">
+              <button
+                type="submit"
+                className="block w-full rounded-xl bg-black px-3.5 py-2.5 text-center text-sm font-semibold text-white  shadow-sm hover:bg-white hover:text-black hover:ring-black hover:ring-2 "
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+
       </div>
       <Footer />
     </>
