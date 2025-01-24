025472baa804b42d94db4cbca3cc327470735f5f
Home <Phithatsanan@outlook.co.th>
2025-01-24 16:32:49 +0700

    Fix bug

--- robot-monitor-web-next/src/components/AuthLayout.js
+++ robot-monitor-web-next/src/components/AuthLayout.js
@@ -26,11 +26,11 @@ function ProtectedComponent({ children }) {
     }
   }, [user, loading, router, pathname]);
 
-  if (loading) {
+  if (!loading) {
     return (
-      <main className="grid h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center justify-items-center">
-          <img src="/monitor.png" alt="Loading..." className="w-32 h-32 animate-spin justify-center" />
+      <main className="flex h-screen items-center justify-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <img src="/monitor.png" alt="Loading..." className="w-32 h-32 animate-spin mx-auto" />
           <h1 className="mt-10 text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
             Hold On
           </h1>
@@ -41,6 +41,7 @@ function ProtectedComponent({ children }) {
       </main>
     );
   }
+
 
   if (publicRoutes.includes(pathname)) {
     return <>{children}</>;
